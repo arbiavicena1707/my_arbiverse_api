6b5747c9ce5293f7c383053935b3095e6267ab47
arbi avicena <arbi.avicena2020@gmail.com>
2025-10-22 16:46:16 +0700

    test

--- routes/kasir.router.js
+++ routes/kasir.router.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../public"));
+    cb(null, path.join(__dirname, "../public")) ;
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
