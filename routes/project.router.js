6d7f0b90418f85f14f6e75d64155aab9916c955c
arbi avicena <arbi.avicena2020@gmail.com>
2025-10-24 22:01:37 +0700

    fix projectr

--- routes/project.router.js
+++ routes/project.router.js
@@ -5,12 +5,16 @@ import {
   addProject,
   deleteProject,
 } from "../controllers/project.controller.js";
+import multer from "multer";
 
+const upload = multer();
 const router = express.Router();
 
 router.get("/", getProjects);
 router.get("/:id", getProjectById);
-router.post("/", addProject);
-router.delete("/:id", deleteProject) ;
+
+
+router.post("/", upload.none(), addProject);
+router.delete("/:id", deleteProject);
 
 export default router;
